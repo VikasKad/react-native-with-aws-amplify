0190ec2b5ba81eab10aad88913c9c22c86b26b64
Vikas <vikasrkad@gmail.com>
2022-09-17 21:20:08 +0530

    modularised code

--- App.js
+++ App.js
@@ -1,75 +1,13 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Image } from 'react-native';
-import { Entypo,
-  AntDesign,
-  FontAwesome5,
-  MaterialCommunityIcons,} from "@expo/vector-icons";
-import LikeImage from "./assets/images/like.png";
-const post = {
-  id: "p1",
-  createdAt: "19 m",
-  User: {
-    id: "u1",
-    image:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
-    name: "Vadim Savin",
-  },
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
-  numberOfLikes: 11,
-  numberOfShares: 2,
-};
+import FeedPost from "./src/components/FeedPost";
 export default function App() {
   return (
     <View style={styles.container}>
-    <View style={styles.post}>
-      <View style={styles.header}>
-        <Image     
-        source={{ uri: post.User.image }}
-        style={styles.profileImage} 
-        />
-        <View>
-          <Text style={styles.name}>{post.User.name}</Text>
-          <Text style={styles.subtitle}>{post.createdAt}</Text>
-        </View>
-          <Entypo
-          name="dots-three-horizontal"
-          size={18}
-          color="gray"
-          style={styles.icon}
-  />
-      </View>
-      <Text style={styles.description}>{post.description}</Text>
-        {post.image && (
-        <Image
-          source={{ uri: post.image }}
-          style={styles.image}
-          resizeMode="cover"
-        />
-        )}
-            {/* Post footer with likes and button */}
-      <View style={styles.footer}>
-        {/* Stats row */}
-        <View style={styles.statsRow}>
-          <Image source={LikeImage} style={styles.likeIcon} />
-          <Text style={styles.likedBy}>
-            Elon Musk and {post.numberOfLikes} others
-          </Text>
-          <Text style={styles.shares}>{post.numberOfShares} shares</Text>
-        </View>
-      </View>
-      {/* Button container */}
-      <View style={styles.buttonRow}>
-        {/* Like Button */}
-          <View style={styles.iconButton}>
-            <AntDesign name="like2" size={18} color="gray" />
-            <Text style={styles.iconButtonText}>Like</Text>
-          </View>
-
-      </View>
-  </View>
-  <StatusBar style="auto" />
+      <FeedPost />
+      <FeedPost />
+      <StatusBar style="auto" />
     </View>
   );
 }
